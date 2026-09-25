3b01521038aa9e27edbcdb7b4d69185a2efa85c0
= <=>
2026-09-25 19:49:54 +0200

    ra2_01

---
+++ ra2_01.js
@@ -0,0 +1,11 @@
+titulo = "Interstellar"
+director = "Christopher Nolan"
+anio = 2014
+precio = 3.99
+disponible = true
+valoracion = 4.8
+
+console.log(`${titulo}, ${director}, ${anio}, ${precio}, ${disponible}, ${valoracion}`);
+console.log(`Precio de dos alquileres: ${precio*2}`);
+console.log(`Precio de dos alquileres: ${(3*0.50)*precio}`);
+console.log(`La película ${titulo}, cuyo director es ${director}, se estrenó en el año ${anio}, su precio es ${precio}, está ${disponible} y tiene valoración de ${valoracion}`);
